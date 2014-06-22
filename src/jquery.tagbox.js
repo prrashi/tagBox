263383d02ca3df5e9d38be5319166d8b8c839ec3
prrashi <prashanth.pamidi@gmail.com>
2014-06-22 21:32:40 +0530

    Supporting Next Lines in a proper way

--- src/jquery.tagbox.js
+++ src/jquery.tagbox.js
@@ -22,6 +22,23 @@
       BKSP   = 8,
       ESC    = 27;
 
+  function addEditable ($container, text, isHtml) {
+
+    text = text || "";
+
+    $container.children("span.tagbox-wg").removeAttr("contenteditable");
+
+    return $("<span/>").attr("contenteditable", "true")
+                       .addClass("tagbox-wg")
+                       [isHtml? "html": "text"](text)
+                       .appendTo($container);
+
+  }
+
+  function removeEditable () {
+
+  }
+
   function _tagBox ($textarea, options) {
 
     var autoFillData = options.autoFillData;
@@ -37,26 +54,38 @@
     var $tagbox = $("<div/>").addClass("tagbox tagbox-input")
                              .prependTo($container);
 
-    var $currentEditable = $("<span/>").attr("contenteditable", "true")
-                                       .addClass("tagbox-wg")
-                                       .text(existingVal)
-                                       .prependTo($tagbox);
+    var $currentEditable = addEditable($tagbox, existingVal);
 
-    var focus_current_editable = function () {
+    var focusCurrentEditable = function () {
 
       $currentEditable.focus();
     };
 
     /* Event Handlers */
-    $container.on("click", focus_current_editable)
+    $container.on("click", focusCurrentEditable)
               .on("keyup", "[contenteditable='true']", function (e) {
-              
+
+              })
+              .on("keydown", "[contenteditable='true']", function (e) {
+
                 var key = e.which || e.keyCode;
 
-                if (key === ENTER) {
-                
+                if (key === RETURN) {
+
                   e.preventDefault();
 
+                  var $br = $("<br/>");
+
+                  if($currentEditable.text().length === 0){
+
+                    $br.insertBefore($currentEditable);
+                  }else {
+
+                    $br.appendTo($tagbox);
+                    $currentEditable = addEditable($tagbox);
+                  }
+
+                  focusCurrentEditable();
                 }
               });
   }
